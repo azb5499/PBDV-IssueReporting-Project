9ebe192b9aeae5ac005efb7994a019a377e02f34
azb5499 <161161519+azb5499@users.noreply.github.com>
2024-03-06 21:59:28 +0200

    fixed issue with boostrap not loading

--- main.py
+++ main.py
@@ -27,6 +27,23 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db.init_app(app)
 app.config['SECRET_KEY'] = 'your_secret_key'
 boostrap = Bootstrap5(app)
+login_manager = LoginManager(app)
+
+
+# class Admin(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     # Add other fields as necessary
+#
+#
+# class Technician(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     # Add other fields as necessary
+#
+#
+# class Student(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     # Add other fields as necessary
+
 
 @app.route('/')
 def display_home():
@@ -57,16 +74,52 @@ def display_login(user):
             user_info = 'The info of the student taken from the database'
             return redirect(url_for('display_student_dashboard', user=user_info))
         return render_template('login.html', form=form)
+    elif user == '3':
+        # this will be technician login
+        pass
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def display_registration():
+def display_student_registration():
+    form = forms.StudentRegistration()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        password = generate_password_hash(form.password.data, salt_length=8)
+        # try:
+        #     pass
+
+        # this code block will be for checking if a user already exists in a database and adding if they dont exist.
+        flash('This email already exists!')
+        return redirect(url_for('display_student_registration'))
+
+    return render_template('register.html', form=form)
+
+
+@app.route('/register_technician', methods=['GET', 'POST'])
+def display_technician_registration():
     return render_template('register.html')
 
 
 @app.route('/dashboard/<email>')
 def display_dashboard(email: str):
     return render_template('dashboard.html')
+
+
+@app.route('/viewIssue', methods=['GET'])
+def display_issue():
+    return 'View issue'
+
+
+@app.route('/addIssue', methods=['GET', 'POST'])
+def display_add():
+    return 'Create issue'
+
+
+@app.route('/forgotPassword', methods=['GET', 'POST'])
+@app.route('/resetPassword', methods=['GET', 'POST'])
+def display_resetpassword():
+    return 'Reset password'
 
 
 if __name__ == "__main__":
